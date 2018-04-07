6b6463a5951aedc1e1e26b916db44a4245702b7f
Lucia Hill-Rains <luciahillrains@outlook.com>
2018-04-07 05:25:23 -0500

    added ProjectMLoginService

---
+++ ProjectMLoginService/src/main/java/com/team2600/projectm/login/ProjectMLoginService/ProjectMLoginServiceApplication.java
@@ -0,0 +1,12 @@
+package com.team2600.projectm.login.ProjectMLoginService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMLoginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMLoginServiceApplication.class, args);
+	}
+}
---
+++ ProjectMLoginService/src/test/java/com/team2600/projectm/login/ProjectMLoginService/ProjectMLoginServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.team2600.projectm.login.ProjectMLoginService;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectMLoginServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
